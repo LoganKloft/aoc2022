080a0013f1d044043fd11deec5092e2cde736180
Logan Kloft <lkloft@outlook.com>
2025-07-27 10:03:44 -0400

    Complete day 5

--- day_4/puzzle2.py
+++ day_4/puzzle2.py
@@ -19,11 +19,23 @@ class CleaningPair():
             return True
         return False
     
+    def hasOverlap(self) -> bool:
+        if self.cleaningAssignmentA.startingSectionId >= self.cleaningAssignmentB.startingSectionId and self.cleaningAssignmentA.startingSectionId <= self.cleaningAssignmentB.endingSectionId:
+            return True
+        if self.cleaningAssignmentA.endingSectionId >= self.cleaningAssignmentB.startingSectionId and self.cleaningAssignmentA.endingSectionId <= self.cleaningAssignmentB.endingSectionId:
+            return True
+        if self.cleaningAssignmentB.startingSectionId >= self.cleaningAssignmentA.startingSectionId and self.cleaningAssignmentB.startingSectionId <= self.cleaningAssignmentA.endingSectionId:
+            return True
+        if self.cleaningAssignmentB.endingSectionId >= self.cleaningAssignmentA.startingSectionId and self.cleaningAssignmentB.endingSectionId <= self.cleaningAssignmentA.endingSectionId:
+            return True
+        
+        return False
+
 answer = 0
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.txt")) as puzzleInput:
     for cleaningPairString in puzzleInput:
         cleaningPair = CleaningPair(cleaningPairString)
-        if cleaningPair.hasFullOverlap():
+        if cleaningPair.hasOverlap():
             answer += 1
 
-print(f"Day 4 Puzzle 1: {answer}")
+print(f"Day 4 Puzzle 2: {answer}")
---
+++ day_5/puzzle1.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+import os
+
+class Crate():
+    def __init__(self, symbol: str) -> None:
+        self.symbol = symbol
+
+class Stack():
+    def __init__(self) -> None:
+        self.crates: list[Crate] = []
+
+    def pop(self) -> Crate:
+        return self.crates.pop()
+
+    def preappend(self, crate: Crate) -> None:
+        self.crates.insert(0, crate)
+    
+    def append(self, crate: Crate) -> None:
+        self.crates.append(crate)
+    
+    def empty(self) -> bool:
+        return not bool(self.crates)
+    
+    def top(self) -> Crate:
+        return self.crates[-1]
+
+
+class Command(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def do(self) -> None:
+        pass
+
+class MoveCommand(Command):
+    def __init__(self, leftStack: Stack, rightStack: Stack, numCrates: int) -> None:
+        self.leftStack: Stack = leftStack
+        self.rightStack: Stack = rightStack
+        self.numCrates: int = numCrates
+    
+    def do(self) -> None:
+        for _ in range(self.numCrates):
+            self.rightStack.append(self.leftStack.pop())
+
+stacks: list[Stack] = []
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.txt")) as puzzleInput:
+    for puzzleLine in puzzleInput:
+
+        if puzzleLine.startswith("move"):
+            moveLine = puzzleLine.split()
+            MoveCommand(stacks[int(moveLine[3]) - 1], stacks[int(moveLine[5]) - 1], int(moveLine[1])).do()
+        
+        if "[" in puzzleLine:
+            symbolIdx = 1
+            while symbolIdx < len(puzzleLine):
+                stackIdx = symbolIdx // 4
+                if len(stacks) <= stackIdx:
+                    stacks.append(Stack())
+
+                if puzzleLine[symbolIdx] == " ":
+                    symbolIdx += 4
+                    continue
+                
+                stacks[stackIdx].preappend(Crate(puzzleLine[symbolIdx]))
+                symbolIdx += 4
+
+answer = ""
+for stack in stacks:
+    if not stack.empty():
+        answer += stack.top().symbol
+
+print(f"Day 5 Puzzle 1: {answer}")
